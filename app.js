880b7dc7880b94cf276233609a7a28beea3440ee
Atul Kumar <40385822+Atulya1@users.noreply.github.com>
2023-04-08 17:42:01 -0400

    mongo connections made (#1)
    Co-authored-by: Atul Kumar <atulkumar@atuls-mbp.lan>

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb+srv://webdevproject:chetanaatul@travel-plus.jiabsqz.mongodb.net/tuiter?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
