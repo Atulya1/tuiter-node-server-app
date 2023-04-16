e3b171f7167de91708aa9bf5e2d8303c80b1b79c
Atul Kumar <atulkumar@Atuls-MacBook-Pro.local>
2023-04-16 15:57:49 -0400

    user apis added

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import UserDetailsController from "./controllers/tuits/userDetails-controller.js";
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb+srv://webdevproject:chetanaatul@travel-plus.jiabsqz.mongodb.net/tuiter?retryWrites=true&w=majority'
+    || 'mongodb+srv://webdevproject:chetanaatul@travel-plus.jiabsqz.mongodb.net/wanderer?retryWrites=true&w=majority'
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
@@ -15,4 +16,6 @@ app.use(express.json());
 TuitsController(app);
 HelloController(app)
 UserController(app)
+UserDetailsController(app)
+
 app.listen(process.env.PORT || 4000);
---
+++ controllers/tuits/userDetails-controller.js
@@ -0,0 +1,46 @@
+import * as userDetailsDao from "../../tuits/userDetails-dao.js";
+import axios from "axios";
+import * as tuitsDao from "../../tuits/tuits-dao.js";
+
+const findUser  = async (req, res) => {
+    const user = await userDetailsDao.findUser();
+    res.json(user);
+}
+
+const addUser = async (req, res) => {
+    const newUser = req.body;
+    console.log(newUser);
+    const insertedUser = await userDetailsDao
+        .createUser(newUser);
+    res.json(insertedUser);
+}
+
+const updateUser = async (req, res) => {
+    const userIdToUpdate = req.params.userId;
+    const updates = req.body;
+    const status = await userDetailsDao
+        .updateUser(userIdToUpdate,
+            updates);
+    res.json(status);
+
+}
+const deleteUser = async (req, res) => {
+    const deleteUser = req.params.userId;
+    const status = await userDetailsDao
+        .deleteUser(deleteUser);
+    res.json(status);
+}
+
+const findUserById  = async (req, res) => {
+    const userId = req.params.userId;
+    console.log(userId);
+    const userDetails = await userDetailsDao.findUserById(userId)
+    res.json(userDetails);
+}
+export default (app) => {
+    app.get('/api/user/getUser', findUser);
+    app.get('/api/user/getUser/:userId',findUserById);
+    app.post('/api/user/addUser',addUser);
+    app.put('/api/user/updateUser/:userId',updateUser);
+    app.delete('/api/user/deleteUser/:userId',deleteUser);
+}
